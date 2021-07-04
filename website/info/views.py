7c90037c0eac27dbc3681e3e39997d81943d93d0
EvaBruh <alterede4@gmail.com>
2021-07-04 13:27:21 +0300

    Create Category-menu(links) and display Date/Category on posts

--- website/info/views.py
+++ website/info/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render
 from .models import *
-# Create your views here.
+
+
 menu = [{'title': 'О сайте', 'url_name': 'about'},
         {'title': 'Добавить статью', 'url_name': 'add_page'},
         {'title': 'Обратная связь', 'url_name': 'contact'},
@@ -10,11 +11,16 @@ menu = [{'title': 'О сайте', 'url_name': 'about'},
 
 def index(request):
     posts = Post.objects.all()
+    cats = Category.objects.all()
+
     context = {
         'posts': posts,
+        'cats': cats,
         'menu': menu,
         'title': 'Главная',
+        'cat_selected': 0,
     }
+
     return render(request, 'info/index.html', context=context)
 
 
@@ -36,6 +42,24 @@ def login(request):
 
 def show_post(request, post_id):
     return HttpResponse(f'Post with post id = {post_id}')
+
+
+def show_category(request, cat_id):
+    posts = Post.objects.filter(cat_id=cat_id)
+    cats = Category.objects.all()
+
+    if len(posts) == 0:
+        raise Http404()
+
+    context = {
+        'posts': posts,
+        'cats': cats,
+        'menu': menu,
+        'title': 'Отображение по рубрикам',
+        'cat_selected': cat_id,
+    }
+
+    return render(request, 'info/index.html', context=context)
 
 
 def PageNotFound(request, exception):
